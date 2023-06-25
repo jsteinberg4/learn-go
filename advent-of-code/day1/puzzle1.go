4a2d11583f537f23c4f8e8d7f4346520d72ab7df
Jesse Steinberg <jessesteinberg01@gmail.com>
2023-06-24 21:34:59 -0400

    advent of code 2022 golang progress
    - day1 puzzle 1 complete
    - day1 puzzle 2 started
    - experiments with slices & other Golang features

--- advent-of-code/day1/puzzle1.go
+++ advent-of-code/day1/puzzle1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"container/list"
 	"fmt"
 	"os"
 	"strconv"
@@ -9,6 +10,39 @@ import (
 
 // const FILE_INPUT = "input.txt"
 const FILE_INPUT = "/Users/jesse/Projects/Golang/learn-go/advent-of-code/day1/input.txt"
+
+func main() {
+	// puzzle1()
+	var puzzle int
+	puzzle, _ = strconv.Atoi(os.Args[1])
+	switch puzzle {
+	case 1:
+		puzzle1()
+	case 2:
+		puzzle2()
+	case 3:
+		// NOTE: This section just tests out array slices in Go
+		var arr [3]int
+		arrayMuteTest(arr[:])
+		for i := 0; i < len(arr); i++ {
+			fmt.Println(arr[i])
+		}
+	default:
+		fmt.Println("Invalid puzzle number")
+	}
+}
+
+func arrayMuteTest(array []int) {
+	array[0] = 1
+	array[1] = 2
+	array[3] = -1
+}
+
+func printList(myList *list.List) {
+	for e := myList.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+	}
+}
 
 func panicOnError(e error) {
 	if e != nil {
@@ -38,7 +72,7 @@ func maxInt(a, b int) int {
 	return b
 }
 
-func main() {
+func puzzle1() {
 	var (
 		file *os.File
 		err  error
@@ -70,4 +104,30 @@ func main() {
 	mostCalories = maxInt(mostCalories, curCalorieSum)
 	fmt.Printf("Most calories: %d\n", mostCalories)
 	file.Close()
+}
+
+func puzzle2() {
+	// Find top 3 vals
+
+	// WARN: Does not work! Arrays are static in Go. No(?) builtin list
+	// Simple but inefficient:
+	// 1) Sum all people in file. Save each to an array
+	// 2) Sort the array in descending order
+	// 3) Sum top 3 indices
+	// TODO: Improved method, use 3 element array
+	var (
+		file    *os.File
+		scanner *bufio.Scanner
+		err     error
+		// elfSums [3]int
+	)
+
+	// Load file into memory
+	file, err = os.Open(FILE_INPUT)
+	panicOnError(err)
+	scanner = bufio.NewScanner(file)
+
+	for scanner.Scan() {
+
+	}
 }
