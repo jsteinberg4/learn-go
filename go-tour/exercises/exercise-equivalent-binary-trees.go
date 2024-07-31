087897906b342d1af8d78068b4ec726db0ca4813
Jesse Steinberg <jessesteinberg01@gmail.com>
2024-07-30 22:04:07 -0700

    miscellaneous

---
+++ go-tour/exercises/exercise-equivalent-binary-trees.go
@@ -0,0 +1,28 @@
+package exercises
+
+import (
+	"fmt"
+
+	"golang.org/x/tour/tree"
+)
+
+// type Tree struct {
+//     Left  *Tree
+//     Value int
+//     Right *Tree
+// }
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	return false
+}
+
+func TreeMain() {
+	fmt.Println("Trees exercise")
+}
--- go-tour/main.go
+++ go-tour/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+
 	"github.com/jsteinberg4/learn-go/go-tour/exercises"
 )
 
 func main() {
-	exercises.ColorImage()
+	exercise := flag.String("exercise", "none", "exercise name")
+	flag.Parse()
+
+	switch *exercise {
+	case "tree":
+		exercises.TreeMain()
+	default:
+		fmt.Println("Unrecognized exercise name " + *exercise)
+	}
 }
